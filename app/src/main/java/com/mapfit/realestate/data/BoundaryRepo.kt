c6fe76a5362c9524f244c2cfbfc3056e038bc489
dogangulcan <dgngulcan@gmail.com>
2018-04-20 18:24:35 -0400

    add geojson and parsing

--- app/src/main/java/com/mapfit/realestate/data/BoundaryRepo.kt
+++ app/src/main/java/com/mapfit/realestate/data/BoundaryRepo.kt
@@ -6,7 +6,6 @@ import com.mapfit.android.Mapfit
 import com.mapfit.android.geometry.LatLng
 import com.mapfit.realestate.model.GeoJson
 import com.mapfit.realestate.model.Neighborhood
-import com.mapfit.realestate.model.RealEstate
 import com.squareup.moshi.Moshi
 import kotlinx.coroutines.experimental.launch
 import java.io.IOException
@@ -15,178 +14,31 @@ import java.io.InputStream
 
 class BoundaryRepo {
 
-    private val neighborhoodNames = listOf(
-        "little italy",
-        "east village",
-        "murray hill",
-        "stuyvesant town",
-        "washington heights",
-        "central harlem",
-        "soho",
-        "spanish harlem",
-        "financial district",
-        "tribeca",
-        "morningside heights",
-        "hell's kitchen",
-        "flatiron district",
-        "midtown west",
-        "midtown east",
-        "central park",
-        "greenwich village",
-        "battery park city",
-        "chinatown",
-        "lower east side",
-        "gramercy",
-        "west side",
-        "west village",
-        "inwood",
-        "hamilton heights",
-        "noho",
-        "two bridges",
-        "nolita",
-        "kips bay",
-        "upper east side",
-        "chelsea",
-        "city hall area"
-    )
-
-
-    fun neighborhoods(): List<List<RealEstate>> {
-
-        val allRealEstates = mutableListOf<List<RealEstate>>()
-
-        val littleItaly = listOf(
-            RealEstate(
-                "apt1",
-                "https://images.unsplash.com/photo-1505873242700-f289a29e1e0f?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=91b874ce453385d8867cc98ee582fee3&auto=format&fit=crop&w=1024&q=80",
-                "$2,400",
-                "199 Hester St, New York, NY 10013",
-                "Little Italy, Manhattan",
-                2,
-                1,
-                350,
-                "July 14th, 2018"
-            ),
-            RealEstate(
-                "apt2",
-                "https://images.unsplash.com/photo-1494526585095-c41746248156?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=fd170b4cebb0b97e6337529754defcf7&auto=format&fit=crop&w=1024&q=80",
-                "$3,200",
-                "85 Wooster St, New York, NY 10012",
-                "Little Italy, Manhattan",
-                2,
-                1,
-                900,
-                "June 16th, 2018"
-            )
-        )
-        allRealEstates.add(littleItaly)
-        val eastVillage = listOf()
-        allRealEstates.add(eastVillage)
-        val murrayHill = listOf()
-        allRealEstates.add(murrayHill)
-        val stuyvesant = listOf()
-        allRealEstates.add(stuyvesant)
-        val washingtonHeights = listOf()
-        allRealEstates.add(washingtonHeights)
-        val centralHarlem = listOf()
-        allRealEstates.add(centralHarlem)
-        val financialDistrict = listOf()
-        allRealEstates.add(financialDistrict)
-        val tribeca = listOf()
-        allRealEstates.add(tribeca)
-        val morningside = listOf()
-        allRealEstates.add(morningside)
-        val hellsKitchen = listOf()
-        allRealEstates.add(hellsKitchen)
-        val flatironDistrict = listOf()
-        allRealEstates.add(flatironDistrict)
-        val midtownWest = listOf()
-        allRealEstates.add(midtownWest)
-        val midtownEeast = listOf()
-        allRealEstates.add(midtownEeast)
-        val centralPark = listOf()
-        allRealEstates.add(centralPark)
-        val greenwichVillage = listOf()
-        allRealEstates.add(greenwichVillage)
-        val batteryParkCity = listOf()
-        allRealEstates.add(batteryParkCity)
-        val chinatown = listOf()
-        allRealEstates.add(chinatown)
-        val lowerEastSide = listOf()
-        allRealEstates.add(lowerEastSide)
-        val gramercy = listOf()
-        allRealEstates.add(gramercy)
-        val westSide = listOf()
-        allRealEstates.add(westSide)
-        val westVillage = listOf()
-        allRealEstates.add(westVillage)
-        val inwood = listOf()
-        allRealEstates.add(inwood)
-        val hamiltonHeights = listOf()
-        allRealEstates.add(hamiltonHeights)
-        val noho = listOf()
-        allRealEstates.add(noho)
-        val twoBridges = listOf()
-        allRealEstates.add(twoBridges)
-        val nolita = listOf()
-        allRealEstates.add(nolita)
-        val kipsBay = listOf()
-        allRealEstates.add(kipsBay)
-        val upperEastSide = listOf()
-        allRealEstates.add(upperEastSide)
-        val chelsea = listOf(
-            RealEstate(
-                "apt1",
-                "https://images.unsplash.com/photo-1505873242700-f289a29e1e0f?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=91b874ce453385d8867cc98ee582fee3&auto=format&fit=crop&w=1024&q=80",
-                "$2,400",
-                "57 Mercer St, New York, NY 10013",
-                "Chelsea, Manhattan",
-                1,
-                1,
-                700,
-                "June 14th, 2018"
-            ),
-            RealEstate(
-                "apt2",
-                "https://images.unsplash.com/photo-1494526585095-c41746248156?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=fd170b4cebb0b97e6337529754defcf7&auto=format&fit=crop&w=1024&q=80",
-                "$3,200",
-                "85 Wooster St, New York, NY 10012",
-                "Chelsea, Manhattan",
-                2,
-                1,
-                900,
-                "June 16th, 2018"
-            )
-        )
-        allRealEstates.add(chelsea)
-        val cityHallArea = listOf()
-        allRealEstates.add(cityHallArea)
-
-        return allRealEstates
-    }
-
+    private val estateRepo = RealEstateRepo()
 
     fun getNeighborhoods(): LiveData<List<Neighborhood>> {
         val data = MutableLiveData<List<Neighborhood>>()
         launch {
 
             val geoJson = parseGeoJsonFromAssets()
+            val realEstates = estateRepo.getRealEstates()
 
             geoJson?.let {
                 var index = -1
 
                 val neighborhoods: List<Neighborhood> =
-                    neighborhoodNames.map {
+                    geoJson.features.map { feature ->
                         index++
-
                         try {
+
                             Neighborhood(
-                                it,
+                                feature.properties.name,
                                 parseDoublesIntoLatLngs(geoJson.features[index].geometry.coordinates),
-                                emptyList()
+                                realEstates[index]
                             )
                         } catch (e: Exception) {
-                            Neighborhood(it, emptyList(), emptyList())
+                            e.printStackTrace()
+                            Neighborhood()
                         }
                     }
 
@@ -197,17 +49,20 @@ class BoundaryRepo {
         return data
     }
 
-    private fun parseDoublesIntoLatLngs(doubles: List<List<List<Double>>>): List<List<LatLng>> {
+    private fun parseDoublesIntoLatLngs(doubles: List<List<List<List<Double>>>>): List<List<LatLng>> {
         val latLngs = mutableListOf<MutableList<LatLng>>()
 
         doubles.forEach {
-            val outerList = mutableListOf<LatLng>()
 
             it.forEach {
-                outerList.add(LatLng(it[1], it[0]))
+                val outerList = mutableListOf<LatLng>()
+//                val innerList =mutableListOf<LatLng>()
+                it.forEach {
+                    outerList.add(LatLng(it[1], it[0]))
+                }
+                latLngs.add(outerList)
             }
 
-            latLngs.add(outerList)
         }
 
         return latLngs
